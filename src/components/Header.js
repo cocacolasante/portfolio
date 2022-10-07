7b05c90af9d7283dca5b61c4d7249d2f05e4e6cf
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-07 07:54:59 -0400

    working links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <div className='header__content'>
                 <h1>Hi, I'm Anthony Colasante</h1>
                 <p>Blockchain Developer</p>
-                <button href="https://www.linkedin.com/in/anthony-colasante-5ba76693/" className='button'>Get In Touch</button>
+                <a href="mailto:colasantecoding@gmail.com"><button  className='button'>Get In Touch</button></a>
             </div>
         </section>
     );
--- src/components/Social.js
+++ src/components/Social.js
@@ -3,7 +3,7 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="" className="button">colasantecoding@gmail.com</a>
+            <a href="mailto:colasantecoding@gmail.com"><button  className='button'>Get In Touch</button></a>
             <a href="https://twitter.com/cocacolasante" className="button">Twitter</a>
             <a href="https://www.linkedin.com/in/anthony-colasante-5ba76693/" className="button">LinkedIn</a>
             <a href="https://github.com/cocacolasante" className="button">GitHub</a>
