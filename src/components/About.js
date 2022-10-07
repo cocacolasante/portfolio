085c71e1a6a560fd38ccf201c9880af3e66b07e3
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-07 18:03:56 -0400

    update about me

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                 Hi, my name is Anthony, a DApp smart contract developer. Like most others in this space,
                 I am fascinated by this technoloy and what it means for our future as a whole. 
                 Between digital ownership, true digital uniquenesss/representation of real world items,
-                DAOs, and all the other awesome technologies, the future of the internet is bright. 
+                DAOs, and all the other awesome technologies, the future of a new Decentralized Internet is bright. 
                 
                 And I look forward to doing my part to help build it!
             </p>
