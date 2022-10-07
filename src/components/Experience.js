f14eab04b306275f2a28ff423073c4149fa96c87
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-06 21:41:06 -0400

    finished portfolio

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,25 +5,18 @@ const Experience = () => {
 
             <ul>
                 <li>
-                    <i>Clayton State University</i>, 2014-2018
+                    <i>Colorado State University Global</i>, 2022-present
                     <ul>
-                        <li>Graduated with 3.00 GPA</li>
+                        <li>Web Application Certificate</li>
                     </ul>
                 </li>
                 <li>
-                    <i>TechFlow</i> - <strong>Blockchain Developer Intern</strong>, 2018-2019
+                    <i>DApp University</i>, 2022
                     <ul>
-                        <li>Studied DApp development</li>
-                        <li>Assisted Research &amp; Development on Web3 Products</li>
+                        <li>Created full production crypto marketplace</li>
                     </ul>
                 </li>
-                <li>
-                    <i>SpiderChain</i> - <strong>Blockchain Developer</strong>, 2019-2022
-                    <ul>
-                        <li>Contributed to smart contract development on client's platform</li>
-                        <li>Peer reviewed &amp; Audited Solidity code</li>
-                    </ul>
-                </li>
+                
             </ul>
         </div>
     );
--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="" className="button">john@email.com</a>
-            <a href="https://twitter.com/" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
-            <a href="https://github.com/" className="button">GitHub</a>
+            <a href="" className="button">colasantecoding@gmail.com</a>
+            <a href="https://twitter.com/cocacolasante" className="button">Twitter</a>
+            <a href="https://www.linkedin.com/in/anthony-colasante-5ba76693/" className="button">LinkedIn</a>
+            <a href="https://github.com/cocacolasante" className="button">GitHub</a>
         </div>
     );
 }
