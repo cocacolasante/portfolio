d8e371ec2dd9dfe9052f1f7421fd544ac4d63793
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-06 21:33:55 -0400

    portfolio site

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,17 @@
 // Import Assets
-import profile from '../assets/profile.png';
+
 
 const About = () => {
     return (
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+                Hi, my name is Anthony, a DApp smart contract developer. Like most others in this space,
+                I am fascinated by this technoloy and what it means for our future as a whole. 
+                Between digital ownership, true digital uniquenesss/representation of real world items,
+                DAOs, and all the other awesome technologies, the future of the internet is bright. 
+                
+                And I look forward to doing my part to help build it!
             </p>
         </section>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 // Import Assets
-import profile from '../assets/profile.png';
+
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGbTgNFL-LfBg/profile-displayphoto-shrink_800_800/0/1577986340037?e=1670457600&v=beta&t=sH8gaSyYZZ48VVFmlz0YrKch0-YFhAFusP_r_ZBL-8E" alt="John Doe" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm Anthony Colasante</h1>
                 <p>Blockchain Developer</p>
                 <button className='button'>Get In Touch</button>
             </div>
